e31ed1c096b35390fa3ac3e6d63b4ce8a39cd896
Aaron Zimmermann <aaron.zimmermann98@googlemail.com>
2018-03-14 13:51:14 +0100

    PlayerControllerScript

--- Assets/Scripts/Movement/PlayerController.cs
+++ Assets/Scripts/Movement/PlayerController.cs
@@ -4,13 +4,73 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    #region Var's
+    public float moveSpeed = 10f;  // Player Movement Speed
+    public float rotateSpeed = 100f; // Camera Rotation Speed
+
+    public float CameraFOV = 75.0f;  // Camera Field of View
+    #endregion
+
+    void Update()
+    {
+
+        // Sprint
+        #region Sprint   
+        
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            // higher move speed while running
+            moveSpeed = 20f;
+
+            // Highers the Camera Field of view while sprinting
+            if (CameraFOV < 75f)
+            {
+                CameraFOV *= 1.01f; // Smooth transition
+            }
+            if (CameraFOV >= 75)
+            {
+                CameraFOV = 75;
+            }
+        }
+        if (!Input.GetKey(KeyCode.LeftShift))
+        {
+            // normal move speed
+            moveSpeed = 10f;
+
+            // Lowers the Camera FOV back to normal when not sprinting
+            if (CameraFOV > 60f)
+            {
+                CameraFOV *= 0.99f; // Smooth transition
+            }
+            if (CameraFOV <= 60)
+            {
+                CameraFOV = 60;
+            }
+        }
+
+        Camera.main.fieldOfView = CameraFOV;
+
+        #endregion
+
+
+        // Controller
+        #region Controller
+
+        // Keyboard Input - WASD
+        float xInputTranslate = Input.GetAxis("Horizontal");
+        float zInputTranslate = Input.GetAxis("Vertical");
+
+        // Mouse Input
+        float xMouseInput = Input.GetAxis("Mouse X") * rotateSpeed;
+
+        // Movement        
+        transform.localPosition += transform.forward * zInputTranslate * moveSpeed * Time.deltaTime
+                                + transform.right * xInputTranslate * moveSpeed * Time.deltaTime;
+
+        // Rotation
+        transform.Rotate(0.0f, xMouseInput, 0.0f);
+
+        #endregion
+
+    }
 }
