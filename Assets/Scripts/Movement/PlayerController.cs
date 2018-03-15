0e610657c16d45c1bc56c80764812c9c49ea9a08
Aaron Zimmermann <aaron.zimmermann98@googlemail.com>
2018-03-15 09:41:32 +0100

    Crouch
    Player is now able to crouch

--- Assets/Scripts/Movement/PlayerController.cs
+++ Assets/Scripts/Movement/PlayerController.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     #region Var's
+
     private float moveSpeed = 0.0f;
-    [SerializeField] float moveSpeedWalking = 2f;   // Player Walking Speed
-    [SerializeField] float moveSpeedRunning = 4f;   // Player Running Speed
-    [SerializeField] float rotateSpeed = 2.5f;        // Camera Rotation Speed
+    [SerializeField] float moveSpeedCrouching   = 2f;   // Player Crouching Speed
+    [SerializeField] float moveSpeedWalking     = 3f;   // Player Walking Speed
+    [SerializeField] float moveSpeedRunning     = 4f;   // Player Running Speed
+    [SerializeField] float rotateSpeed          = 2.5f; // Camera Rotation Speed
 
-    public float CameraFOV = 75.0f;                 // Camera Field of View
+    private bool crouching  = false;                    // Is player crouching? = True
+    private bool running    = false;                    // Is player running? = True
+
+    private float CameraFOV = 75.0f;                    // Camera Field of View
+    private float CameraYNormal = -0.5f;                // Camera Height while standing
+    private float CameraYCrouch = -1.0f;                // Camera Height while crouching
+    private float CameraY;
+
     #endregion
-
+     
     void Update()
     {
 
@@ -21,46 +30,57 @@ public class PlayerController : MonoBehaviour {
 
         if (Input.GetKey(KeyCode.LeftShift))
         {
-            
-            if (Input.GetAxis("Horizontal") != 0 || (Input.GetAxis("Vertical") != 0))
+
+            if (!crouching)
             {
-                // higher move speed while running
-                moveSpeed = moveSpeedRunning;
 
+                // Player is now running
+                running = true;
 
-                 // Highers the Camera Field of view while sprinting
-                if (CameraFOV < 75f)
+                if (Input.GetAxis("Horizontal") != 0 || (Input.GetAxis("Vertical") != 0))
                 {
-                 CameraFOV *= 1.01f; // Smooth transition
+                    // higher move speed while running
+                    moveSpeed = moveSpeedRunning;
+
+
+                    // Highers the Camera Field of view while sprinting
+                    if (CameraFOV < 75f)
+                    {
+                        CameraFOV *= 1.01f; // Smooth transition
+                    }
+
+                    if (CameraFOV >= 75)
+                    {
+                        CameraFOV = 75;
+                    }
                 }
 
-                if (CameraFOV >= 75)
+                if (Input.GetAxis("Horizontal") == 0 && (Input.GetAxis("Vertical") == 0))
                 {
-                CameraFOV = 75;
+
+                    // normal move speed
+                    moveSpeed = moveSpeedWalking;
+
+                    // Lowers the Camera FOV back to normal when not sprinting
+                    if (CameraFOV > 60f)
+                    {
+                        CameraFOV *= 0.99f; // Smooth transition
+                    }
+
+                    if (CameraFOV <= 60)
+                    {
+                        CameraFOV = 60;
+                    }
                 }
-            }
-
-            if (Input.GetAxis("Horizontal") == 0 && (Input.GetAxis("Vertical") == 0))
-            {
-
-                // normal move speed
-                moveSpeed = moveSpeedWalking;
-
-            // Lowers the Camera FOV back to normal when not sprinting
-            if (CameraFOV > 60f)
-            {
-                CameraFOV *= 0.99f; // Smooth transition
-            }
-
-            if (CameraFOV <= 60)
-            {
-                CameraFOV = 60;
-            }
             }
         }
 
         if (!Input.GetKey(KeyCode.LeftShift))
         {
+
+            // Player not longer running
+            running = false;
+
             // normal move speed
             moveSpeed = moveSpeedWalking;
 
@@ -78,6 +98,56 @@ public class PlayerController : MonoBehaviour {
         }
 
         Camera.main.fieldOfView = CameraFOV;
+            
+
+        #endregion
+
+        // Crouch
+        #region Crouch
+
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+
+            // You can't crouch while running
+            if (running)
+            {
+                return;
+            }
+
+            if (!running)
+            {
+                // Player is now crouching
+                crouching = true;
+
+                if (CameraY > CameraYCrouch)
+                {
+                    CameraY *= 1.05f;
+                }
+
+                if (CameraY <= CameraYCrouch)
+                {
+                    CameraY = CameraYCrouch;
+                }
+            }
+        }
+
+        if (!Input.GetKey(KeyCode.LeftControl))
+        {
+
+            crouching = false;
+
+            if (CameraY < CameraYNormal)
+            {
+                CameraY *= 0.95f;
+            }
+
+            if (CameraY >= CameraYNormal)
+            {
+                CameraY = CameraYNormal;
+            }
+        }
+
+        transform.position = new Vector3(transform.position.x, CameraY, transform.position.z);
 
         #endregion
 
