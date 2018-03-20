99f8030d4f92b730c60a4a762a3aa7fd16267a19
Robin Uth <robin.uth95@gmail.com>
2018-03-20 11:13:11 +0100

    Back to highlight
    old highlight, working

--- Assets/Scripts/Highlight/HighlightObject.cs
+++ Assets/Scripts/Highlight/HighlightObject.cs
@@ -4,24 +4,19 @@ using UnityEngine;
 
 public class HighlightObject : MonoBehaviour
 {
+
     #region Var's
     public float distanceToSee;
     public string ObjectName;
     private Color highlightColor;
     Material originalMaterial, tempMaterial;
     Renderer renderer = null;
-    public Renderer rend;
-
-
-    int key = 0;
 
     #endregion
 
     void Start()
     {
-        rend = GetComponent<Renderer>();
-        rend.material.shader = Shader.Find("Outlined/Silhouette Only");
-                    rend.material.SetFloat("_width", 0.03f);        
+        highlightColor = Color.green;
     }
 
 
@@ -29,26 +24,12 @@ public class HighlightObject : MonoBehaviour
     {
         RaycastHit hitInfo;
         Renderer currRenderer;
-        Renderer rend;
 
         Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
 
         // highlights gameObject if its a collectible and in range of raycast
         if (Physics.Raycast(ray.origin, ray.direction, out hitInfo, distanceToSee) && hitInfo.transform.tag == "collectible")
         {
-
-            // collect object and delete it from scene, adding +1 to inventory
-            if (Input.GetKey(KeyCode.E))
-            {
-                key += 1;
-                Debug.Log(key);
-                Destroy(hitInfo.collider.gameObject);
-            }
-
-            
-        }
-    }
-            /*
             currRenderer = hitInfo.collider.gameObject.GetComponent<Renderer>();
 
             if (currRenderer == renderer)
@@ -62,6 +43,7 @@ public class HighlightObject : MonoBehaviour
                 }
 
             }
+
             if (currRenderer)
                 renderer = currRenderer;
             else
@@ -72,7 +54,6 @@ public class HighlightObject : MonoBehaviour
             tempMaterial = new Material(originalMaterial);
             renderer.material = tempMaterial;
             renderer.material.color = highlightColor;
-
         }
         else
         {
@@ -83,5 +64,4 @@ public class HighlightObject : MonoBehaviour
             }
         }
     }
-    //*/
 }
